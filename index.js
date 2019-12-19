95bd26ffce25a8ce08917d5c564bfbc084f5b520
stackblock <lambdaTL.anthony@gmail.com>
2019-12-18 23:54:56 -0500

    done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace("#", ""));
+
+if (message) {
+  document.querySelector("#message-form").classList.toggle("hide");
+  document.querySelector("#message-show").classList.toggle("hide");
+
+  document.querySelector("h1").innerHTML = message;
+}
+
 document.querySelector("form").addEventListener("submit", event => {
   event.preventDefault();
 
-  const input = document.querySelector("input");
+  document.querySelector("#message-form").classList.toggle("hide");
+  document.querySelector("#link-form").classList.toggle("hide");
 
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+  console.log(linkInput.value);
 });
